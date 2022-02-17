0921d99338cc01d2e3ef94eff466ba86c9dc17fb
Kibria hossain <you@example.com>
2022-02-17 14:55:07 +0600

    all error complete

--- script.js
+++ script.js
@@ -26,42 +26,54 @@ const remainingBalance = remaining.innerText;
 
 
 
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
-                if (typeof(getValueAndFloat('food')) != 'number' || isNaN((getValue('food'))) == true || income.value == '' || getValueAndFloat('food') < 0) {
-                balance.innerText = '';
-                        alert('Please valid number in income box')
-                console.log('This is hapend');        
-        }
         // Total expeses calculates        
                 const sum = getValueAndFloat('food') + getValueAndFloat('rent') +  getValueAndFloat('cloths');
                  expensesText = sum;
-                expenses.innerText = expensesText;
-        // Total balance calculates
+                 expenses.innerText = expensesText;
+          // Total balance calculates
                 const minusResult = parseFloat(income.value) - parseFloat(expenses.innerText);
         balance.innerText = minusResult;
-        // console.log(typeof (parseFloat(income.value)));
-        
-        if (typeof ((parseFloat(income.value))) != 'number' || isNaN(income.value) == true || income.value == '' || parseFloat(income.value) < 0) {
-                balance.innerText = '';
-                alert('Please valid number in income box')
-            
-        }
 
-        // console.log(isNaN(getValue('food')));
-        
+        // --------------------Error problme------------------------------
+        // Income box error
+                if (typeof ((parseFloat(income.value))) != 'number' || isNaN(income.value) == true || income.value == '' || parseFloat(income.value) < 0) {
+                balance.innerText = ''
+                alert('Please valid number in income box')
+        }
+        // Input box error
+
+        if (getValue('food') == '' || getValue('rent') == '' || getValue('cloths') == '') {
+                expenses.innerText = '';
+                balance.innerText = '';
+                alert('Plese fill all input box')
+
+        }  
+                if (isNaN(getValueAndFloat('food')) == true || isNaN(getValueAndFloat('rent')) == true || isNaN(getValueAndFloat('cloths')) == true || (getValueAndFloat('food'))<0 || (getValueAndFloat('rent'))<0 || (getValueAndFloat('cloths'))<0) {
+                expenses.innerText = '';
+                balance.innerText = '';
+                alert('Plese enter valid number in input field')
+        }
+        if (parseFloat(expenses.innerText) >parseFloat( balance.innerText)) {
+                balance.innerText = '';
+                 expenses.innerText = '';
+                alert('Your cost is grater than your income')
+        }
+  
 })
 document.getElementById('save-btn').addEventListener('click', function () {
                 let savingResult = parseFloat(income.value) * (parseFloat(save.value)) / 100;
         
         if (savingResult > balance.innerText ) {
-                 alert('sorry')
+                 alert('You have no enough balance')
                 savingResult = '';
         }
         else if (typeof ((parseFloat(save.value))) != 'number' || isNaN(save.value)==true || save.value == ''|| parseFloat(save.value) <0) {
                 savingAmount.innerText = '';
                 remaining.innerText = '';
-                alert('Plese enter valid number')
+                alert('Please enter a valid number')
         }
         else {
                 
